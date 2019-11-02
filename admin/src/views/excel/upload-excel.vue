bd712430a0ea11fe7ddf7a75735bce6d6fbf4ddb
杨丰畅 <ayang818@qq.com>
2019-11-02 21:19:55 +0800

    完成比赛文件上传

--- admin/src/views/excel/upload-excel.vue
+++ admin/src/views/excel/upload-excel.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="app-container">
-    <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
-    <el-button style="float: right; margin: 1rem 0;" size="mini" type="primary" @click="handleUpload" v-if="tableData.length != 0">
-        确认上传
-    </el-button>
-    <h2 style="text-align: center;" v-if="tableData.length != 0">预览excel</h2>
-    <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
-      <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
-    </el-table>
+    <el-tabs type="border-card">
+      <el-tab-pane label="预览" name="previous">
+        <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
+        <h2 style="text-align: center;" v-if="tableData.length != 0">预览excel</h2>
+        <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
+          <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="上传" name="upload">
+        <el-upload
+          class="upload-demo"
+          :action="$http.defaults.baseURL+'/api/excel/competition'"
+          :headers=getAuthHeaders()
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :before-remove="beforeRemove"
+           multiple
+          :limit="1"
+          :file-list="fileList">
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip"></div>
+        </el-upload>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -20,7 +36,8 @@ export default {
   data() {
     return {
       tableData: [],
-      tableHeader: []
+      tableHeader: [],
+      fileList: []
     }
   },
   methods: {
@@ -30,19 +47,23 @@ export default {
       if (isLt1M) {
         return true
       }
-
       this.$message({
         message: 'Please do not upload files larger than 1m in size.',
         type: 'warning'
       })
       return false
     },
-    handleUpload() {
-      this.$store.dispatch('excel/upload_competition_excel', this.tableData)
-    },
     handleSuccess({ results, header }) {
       this.tableData = results
       this.tableHeader = header
+    },
+    handleRemove(file, fileList) {
+      fileList.remove(file);
+    },
+    handlePreview(file) {
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${ file.name }？`);
     }
   }
 }
